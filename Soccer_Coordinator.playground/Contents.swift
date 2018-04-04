bc6415f1bb51deeeefaca4f3c32bf2ef61c96e29
Desiree' Bryant <desiree@desireebryant.com>
2018-04-04 00:08:18 -0700

    Added Player information

--- Soccer_Coordinator.playground/Contents.swift
+++ Soccer_Coordinator.playground/Contents.swift
@@ -1,7 +1,24 @@
-// Players
+// Soccer Coordinator
 
-// name String, height Double, experience Bool or String, guardian String
-// players [String: Any]
-// teams: Sharks teamSharks, Dragons teamDragons, Raptors teamRaptors
+// Player Information
+let players: [[String: Any]] = [
+    ["playerName": "Joe Smith", "height": 42, "experienced": true, "guardian": "Jim and Jan Smith"],
+    ["playerName": "Jill Tanner", "height": 36, "experienced": true, "guardian": "Clara Tanner"],
+    ["playerName": "Bill Bon", "height": 43, "experienced": true, "guardian": "Sara and Jenny Bon"],
+    ["playerName": "Matt Gill", "height": 40, "experienced": false, "guardian": "Charles and Sylvia Gill"],
+    ["playerName": "Kimmy Stein", "height": 41, "experienced": false, "guardian": "Bill and Hillary Stein"],
+    ["playerName": "Sammy Adams", "height": 45, "experienced": false, "guardian": "Jeff Adams"],
+    ["playerName": "Karl Saygan", "height": 42, "experienced": true, "guardian": "Heather Bledsoe"],
+    ["playerName": "Suzane Greenberg", "height": 44, "experienced": true, "guardian": "Henrietta Dumas"],
+    ["playerName": "Sal Dali", "height": 41, "experienced": false, "guardian": "Gala Dali"],
+    ["playerName": "Joe Kavalier", "height": 39, "experienced": false, "guardian": "Sam and Elaine Kavalier"],
+    ["playerName": "Ben Finkelstein", "height": 44, "experienced": false, "guardian": "Aaron and Jill Finkelstein"],
+    ["playerName": "Diego Soto", "height": 41, "experienced": true, "guardian": "Robin and Sarika Soto"],
+    ["playerName": "Chloe Alaska", "height": 47, "experienced": false, "guardian": "David and Jamie Alaska"],
+    ["playerName": "Arnold Willis", "height": 43, "experienced": false, "guardian": "Claire Willis"],
+    ["playerName": "Phillip Helm", "height": 44, "experienced": true, "guardian": "Thomas Helm and Eva Jones"],
+    ["playerName": "Les Clay", "height": 42, "experienced": true, "guardian": "Wynonna Brown"],
+    ["playerName": "Herschel Krustofski", "height": 45, "experienced": true, "guardian": "Hyman and Rachel Krustofski"]
+]
 
-let player1: [String: Any] = ["name": "Joe Smith", "height": 42, "experienced": true, "guardian": "Jim and Jan Smith"]
+
