4bbfece76ce0f067a1601c50db82f8eef2a93160
Desiree' Bryant <desiree@desireebryant.com>
2018-04-03 23:23:30 -0700

    Added Playground to Project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my first project for the Team Treehouse Tech degree program for iOS.
---
+++ Soccer_Coordinator.playground/Contents.swift
@@ -0,0 +1,7 @@
+// Players
+
+// name String, height Double, experience Bool or String, guardian String
+// players [String: Any]
+// teams: Sharks teamSharks, Dragons teamDragons, Raptors teamRaptors
+
+let player1: [String: Any] = ["name": "Joe Smith", "height": 42, "experienced": true, "guardian": "Jim and Jan Smith"]
