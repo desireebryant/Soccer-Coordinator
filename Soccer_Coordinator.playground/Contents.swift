d6818e3d6a60cae5c51c0f8c01c7795d1a640f69
Desiree' Bryant <desiree@desireebryant.com>
2018-04-12 18:59:24 -0500

    Made revisions based on initial Peer Review

--- Soccer_Coordinator.playground/Contents.swift
+++ Soccer_Coordinator.playground/Contents.swift
@@ -6,36 +6,36 @@ var teamDragons: [[String: Any]] = []
 var teamSharks: [[String: Any]] = []
 
 // Soccor League
-var soccerLeague = (teamRaptors, teamDragons, teamSharks)
-
-// Player Experience
-var experiencedPlayers: [[String: Any]] = []
-var notExperiencedPlayers: [[String: Any]] = []
+var soccerLeague = [teamRaptors, teamDragons, teamSharks]
+var numberOfTeams: Int = soccerLeague.count
 
 // Team Letters
 var letters: [String] = []
 
 // Player Information
 var players: [[String: Any]] = [
-    ["teamName": "team", "playerName": "Joe Smith", "height": 42.0, "isExperienced": true, "guardian": "Jim and Jan Smith"],
-    ["teamName": "team", "playerName": "Jill Tanner", "height": 36.0, "isExperienced": true, "guardian": "Clara Tanner"],
-    ["teamName": "team", "playerName": "Bill Bon", "height": 43.0, "isExperienced": true, "guardian": "Sara and Jenny Bon"],
-    ["teamName": "team", "playerName": "Eva Gordon", "height": 45.0, "isExperienced": false, "guardian": "Wendy and Mike Gordon"],
-    ["teamName": "team", "playerName": "Matt Gill", "height": 40.0, "isExperienced": false, "guardian": "Charles and Sylvia Gill"],
-    ["teamName": "team", "playerName": "Kimmy Stein", "height": 41.0, "isExperienced": false, "guardian": "Bill and Hillary Stein"],
-    ["teamName": "team", "playerName": "Sammy Adams", "height": 45.0, "isExperienced": false, "guardian": "Jeff Adams"],
-    ["teamName": "team", "playerName": "Karl Saygan", "height": 42.0, "isExperienced": true, "guardian": "Heather Bledsoe"],
-    ["teamName": "team", "playerName": "Suzane Greenberg", "height": 44.0, "isExperienced": true, "guardian": "Henrietta Dumas"],
-    ["teamName": "team", "playerName": "Sal Dali", "height": 41.0, "isExperienced": false, "guardian": "Gala Dali"],
-    ["teamName": "team", "playerName": "Joe Kavalier", "height": 39.0, "isExperienced": false, "guardian": "Sam and Elaine Kavalier"],
-    ["teamName": "team", "playerName": "Ben Finkelstein", "height": 44.0, "isExperienced": false, "guardian": "Aaron and Jill Finkelstein"],
-    ["teamName": "team", "playerName": "Diego Soto", "height": 41.0, "isExperienced": true, "guardian": "Robin and Sarika Soto"],
-    ["teamName": "team", "playerName": "Chloe Alaska", "height": 47.0, "isExperienced": false, "guardian": "David and Jamie Alaska"],
-    ["teamName": "team", "playerName": "Arnold Willis", "height": 43.0, "isExperienced": false, "guardian": "Claire Willis"],
-    ["teamName": "team", "playerName": "Phillip Helm", "height": 44.0, "isExperienced": true, "guardian": "Thomas Helm and Eva Jones"],
-    ["teamName": "team", "playerName": "Les Clay", "height": 42.0, "isExperienced": true, "guardian": "Wynonna Brown"],
-    ["teamName": "team", "playerName": "Herschel Krustofski", "height": 45.0, "isExperienced": true, "guardian": "Hyman and Rachel Krustofski"]
+    ["playerName": "Joe Smith", "height": 42.0, "isExperienced": true, "guardian": "Jim and Jan Smith"],
+    ["playerName": "Jill Tanner", "height": 36.0, "isExperienced": true, "guardian": "Clara Tanner"],
+    ["playerName": "Bill Bon", "height": 43.0, "isExperienced": true, "guardian": "Sara and Jenny Bon"],
+    ["playerName": "Eva Gordon", "height": 45.0, "isExperienced": false, "guardian": "Wendy and Mike Gordon"],
+    ["playerName": "Matt Gill", "height": 40.0, "isExperienced": false, "guardian": "Charles and Sylvia Gill"],
+    ["playerName": "Kimmy Stein", "height": 41.0, "isExperienced": false, "guardian": "Bill and Hillary Stein"],
+    ["playerName": "Sammy Adams", "height": 45.0, "isExperienced": false, "guardian": "Jeff Adams"],
+    ["playerName": "Karl Saygan", "height": 42.0, "isExperienced": true, "guardian": "Heather Bledsoe"],
+    ["playerName": "Suzane Greenberg", "height": 44.0, "isExperienced": true, "guardian": "Henrietta Dumas"],
+    ["playerName": "Sal Dali", "height": 41.0, "isExperienced": false, "guardian": "Gala Dali"],
+    ["playerName": "Joe Kavalier", "height": 39.0, "isExperienced": false, "guardian": "Sam and Elaine Kavalier"],
+    ["playerName": "Ben Finkelstein", "height": 44.0, "isExperienced": false, "guardian": "Aaron and Jill Finkelstein"],
+    ["playerName": "Diego Soto", "height": 41.0, "isExperienced": true, "guardian": "Robin and Sarika Soto"],
+    ["playerName": "Chloe Alaska", "height": 47.0, "isExperienced": false, "guardian": "David and Jamie Alaska"],
+    ["playerName": "Arnold Willis", "height": 43.0, "isExperienced": false, "guardian": "Claire Willis"],
+    ["playerName": "Phillip Helm", "height": 44.0, "isExperienced": true, "guardian": "Thomas Helm and Eva Jones"],
+    ["playerName": "Les Clay", "height": 42.0, "isExperienced": true, "guardian": "Wynonna Brown"],
+    ["playerName": "Herschel Krustofski", "height": 45.0, "isExperienced": true, "guardian": "Hyman and Rachel Krustofski"]
 ]
+
+// Calculate number of players
+let numberOfPlayers: Int = players.count
 
 // Sort players based on height - descending
 players.sort(by: {$0["height"] as! Double > $1["height"] as! Double})
@@ -56,92 +56,128 @@ func calcAverageHeight(ofTeam teamBeingCalculated: [[String: Any]]) -> Double {
     return averagePlayerHeight
 }
 
-// Separate players by experience
-for player in players {
-    switch true {
-    case player["isExperienced"] as? Bool: experiencedPlayers.append(player)
-    default: notExperiencedPlayers.append(player)
-    }
-}
-
-// Sort experienced players based on height - decreasing
-experiencedPlayers.sort(by: {$0["height"] as! Double > $1["height"] as! Double})
-// Sort non-experienced players based on height - increasing
-notExperiencedPlayers.sort(by: {$1["height"] as! Double > $0["height"] as! Double})
-
-// Average Height of players
-let averageHeightAllPlayers: Double = calcAverageHeight(ofTeam: players)
-let averageHeightNonExperiencedPlayers: Double = calcAverageHeight(ofTeam: notExperiencedPlayers)
-
-// Separate experienced players into teams by height
-for player in experiencedPlayers {
-    var player = player
-    if (teamRaptors.count <= teamDragons.count) && (teamRaptors.count <= teamSharks.count) {
-        player.updateValue("Raptors", forKey: "teamName")
-        teamRaptors.append(player)
-    }
-    else if (teamDragons.count <= teamRaptors.count) && (teamDragons.count <= teamSharks.count) {
-        player.updateValue("Dragons", forKey: "teamName")
-        teamDragons.append(player)
-    } else if (teamSharks.count <= teamRaptors.count) && (teamSharks.count <= teamDragons.count) {
-        player.updateValue("Sharks", forKey: "teamName")
-        teamSharks.append(player)
-    }
-}
-
-// Added non-experienced players by height
-for player in notExperiencedPlayers {
-    var player = player
-    if (teamRaptors.count <= teamDragons.count) && (teamRaptors.count <= teamSharks.count) {
-        player.updateValue("Raptors", forKey: "teamName")
-        teamRaptors.append(player)
-    } else if (teamDragons.count <= teamRaptors.count) && (teamDragons.count <= teamSharks.count) {
-        player.updateValue("Dragons", forKey: "teamName")
-        teamDragons.append(player)
-    } else if (teamSharks.count <= teamRaptors.count) && (teamSharks.count <= teamDragons.count) {
-        player.updateValue("Sharks", forKey: "teamName")
-        teamSharks.append(player)
-    }
-}
-
-// Average Height of Each Team
-print(calcAverageHeight(ofTeam: teamRaptors))
-print(calcAverageHeight(ofTeam: teamDragons))
-print(calcAverageHeight(ofTeam: teamSharks))
-
-// Create Letters for each player on each team
-func createTeamLetters(forPlayers onTeam: [[String: Any]]) {
-    var letter: String = ""
-    var practiceTime: String = ""
-    var practiceDate: String = ""
+// Separate players to groups by experience and sort each group by height
+func sortPlayers(fromPlayers toGroups: [[String: Any]]) -> (experiencedPlayers: [[String: Any]], notExperiencedPlayers: [[String: Any]]) {
+ 
+    // Player Experience
+    var experiencedPlayers: [[String: Any]] = []
+    var notExperiencedPlayers: [[String: Any]] = []
     
-    for player in onTeam {
-        let playerName = player["playerName"] as! String
-        let guardian = player["guardian"] as! String
-        let teamName = player["teamName"] as! String
-
-        if (teamName == "Raptors") {
-            practiceTime = "1pm"
-            practiceDate = "March 18"
-        } else if (teamName == "Dragons") {
-            practiceTime = "1pm"
-            practiceDate = "March 17"
-        } else if (teamName == "Sharks") {
-            practiceTime = "3pm"
-            practiceDate = "March 17"
+    // Separate players by experience
+    for player in players {
+        
+        guard let isExperienced = player["isExperienced"] as? Bool else {
+            break
         }
         
-        letter = "Hello \(guardian), We are happy to announce that your child, \(playerName), will be playing on team \(teamName) this season. Our first practice will be held on: \(practiceDate), \(practiceTime). Please do not be late. Thanks, Couch Manning."
+        // Separate experienced from not experienced players
+        switch isExperienced {
+        case true: experiencedPlayers.append(player)
+        default: notExperiencedPlayers.append(player)
+        }
+        
+        // Sort experienced players by height - descending
+        experiencedPlayers = experiencedPlayers.sorted(by: {
+            guard let playerHeightTop = $0["height"] as? Double else { return false }
+            guard let playerHeightBottom = $1["height"] as? Double else { return false }
+            return playerHeightTop > playerHeightBottom
+        })
+        
+        // Sort non experienced players by height - ascending
+        notExperiencedPlayers = notExperiencedPlayers.sorted(by: {
+            guard let playerHeightTop = $0["height"] as? Double else { return false }
+            guard let playerHeightBottom = $1["height"] as? Double else { return false }
+            return playerHeightTop < playerHeightBottom
+        })
+
+    }
+
+    return(experiencedPlayers, notExperiencedPlayers)
+}
+
+// Put players in the exp vs not exp groups
+var experiencedPlayers: [[String: Any]] = sortPlayers(fromPlayers: players).experiencedPlayers
+var notExperiencedPlayers: [[String: Any]] = sortPlayers(fromPlayers: players).notExperiencedPlayers
+
+// Calculate how many experienced and not experienced players per team
+let expPlayersPerTeam: Int = (experiencedPlayers.count / numberOfTeams)
+let nonExpPlayersPerTeam: Int = (notExperiencedPlayers.count / numberOfTeams)
+
+// Add players to teams
+func addPlayersToTeams() -> [[String: Any]] {
+    // Placeholder from team members
+    var team: [[String: Any]] = []
+    
+    // Fill experienced players
+    for _ in 0..<expPlayersPerTeam {
+        // As player is added to a team, remove player from experienced players group
+        team.append(experiencedPlayers[experiencedPlayers.count - 1])
+        experiencedPlayers.remove(at: experiencedPlayers.count - 1)
+    }
+    
+    // Fill not experienced players
+    for _ in 0..<nonExpPlayersPerTeam {
+        // As player is added to a team, remove player from not experienced players group
+        team.append(notExperiencedPlayers[notExperiencedPlayers.count - 1])
+        notExperiencedPlayers.remove(at: notExperiencedPlayers.count - 1)
+    }
+   return team
+}
+
+// Add players to each team
+teamRaptors = addPlayersToTeams()
+teamDragons = addPlayersToTeams()
+teamSharks = addPlayersToTeams()
+
+// Average Height of Each Team
+let raptorsAvgHeight: Double = calcAverageHeight(ofTeam: teamRaptors)
+let dragonsAvgHeight: Double = calcAverageHeight(ofTeam: teamDragons)
+let sharksAvgHeight: Double = calcAverageHeight(ofTeam: teamSharks)
+
+print("The average height of team Raptors is \(raptorsAvgHeight).")
+print("The average height of team Dragons is \(dragonsAvgHeight).")
+print("The average height of team Sharks is \(sharksAvgHeight).")
+
+// Create Letters for each player on each team
+func createTeamLetters(forPlayers onTeam: [[String: Any]], teamName tname: String) {
+
+    for player in onTeam {
+        var letter: String = ""
+        var practiceTime: String = ""
+        var practiceDate: String = ""
+        
+        guard let playerName = player["playerName"] as? String else { break }
+        guard let guardian = player["guardian"] as? String else { break }
+
+        // Set team practice times
+        if (tname == "Raptors") {
+            practiceDate = "March 18"
+            practiceTime = "1pm"
+        } else if (tname == "Dragons") {
+            practiceDate = "March 17"
+            practiceTime = "1pm"
+        } else if (tname == "Sharks") {
+            practiceDate = "March 17"
+            practiceTime = "3pm"
+        }
+
+        letter = "\nHello \(guardian), We are happy to announce that your child, \(playerName), will be playing on team \(tname) this season. Our first practice will be held on: \(practiceDate), \(practiceTime). Please do not be late. Thanks, Couch Manning. \n"
         
         letters.append(letter)
     }
     
 }
 
-// Create letters for each team
-createTeamLetters(forPlayers: teamRaptors)
-createTeamLetters(forPlayers: teamDragons)
-createTeamLetters(forPlayers: teamSharks)
+// Create team letters
+createTeamLetters(forPlayers: teamRaptors, teamName: "Raptors")
+createTeamLetters(forPlayers: teamDragons, teamName: "Dragons")
+createTeamLetters(forPlayers: teamSharks, teamName: "Sharks")
 
-// Print letters collection
-print(letters)
+// Print letters for players
+func printLetters() {
+    for letter in letters {
+        print(letter)
+    }
+}
+
+printLetters()
